0544005dea21022b44ecc8aaafdbe572dc45d9ba
azabold-priv <azabold@web.de>
2023-10-02 17:53:39 +0200

    some eslint cleanups

--- main.js
+++ main.js
@@ -1,209 +1,213 @@
-"use strict";
+'use strict';
 
-const utils = require("@iobroker/adapter-core");
-const axios = require("axios");
-const crypto = require("crypto");
+const utils = require('@iobroker/adapter-core');
+const axios = require('axios');
+const crypto = require('crypto');
 
 class SolisCloud extends utils.Adapter {
+  constructor(options) {
+    super({
+      ...options,
+      name: 'solis-cloud',
+    });
+    this.on('ready', this.onReady.bind(this));
+    this.on('stateChange', this.onStateChange.bind(this));
+    this.on('unload', this.onUnload.bind(this));
+    this.stationIds = [];
+  }
 
-    constructor(options) {
-        super({
-            ...options,
-            name: "solis-cloud",
-        });
-        this.on("ready", this.onReady.bind(this));
-        this.on("stateChange", this.onStateChange.bind(this));
-        this.on("unload", this.onUnload.bind(this));
-		this.stationIds = [];
-    }
+  async onReady() {
+    this.log.info('config apiKey: ' + this.config.apiKey);
+    this.log.info('config apiSecret: ' + this.config.apiSecret);
+    this.log.info('config apiUrl: ' + this.config.apiUrl);
+    this.log.info('config refreshInterval: ' + this.config.refreshInterval);
+    this.log.info('config allowedKeys: ' + this.config.allowedKeys);
 
-    async onReady() {
-		
-        this.log.info("config apiKey: " + this.config.apiKey);
-        this.log.info("config apiSecret: " + this.config.apiSecret);
-		this.log.info("config apiUrl: " + this.config.apiUrl);
-		this.log.info("config refreshInterval: " + this.config.refreshInterval);
-		this.log.info("config allowedKeys: " + this.config.allowedKeys);
+    await this.setObjectNotExistsAsync('info.connection', {
+      type: 'state',
+      common: {
+        name: 'Connection Status',
+        type: 'boolean',
+        role: 'indicator.connected',
+        read: true,
+        write: false,
+      },
+      native: {},
+    });
 
-		await this.setObjectNotExistsAsync("info.connection", {
-			type: "state",
-			common: {
-				name: "Connection Status",
-				type: "boolean",
-				role: "indicator.connected",
-				read: true,
-				write: false,
-			},
-			native: {},
-		});
-		
-		this.setState("info.connection", false, true);
-		this.isRateLimited = false;
-		this.refreshInterval = setInterval(() => this.main(), this.config.refreshInterval * 60 * 1000);
-		
-		try {
-			// Liste eingetragene Stations auf
-			var body = JSON.stringify({ pageNo: 1, pageSize: 10 });
-			var path = '/v1/api/userStationList';
-			var response = await this.apiCall(path, body);
-			if (response && response.success == true) {
-				const numberOfRecords = response.data.page.records.length; // Dies gibt die Anzahl der Stations aus
+    this.setState('info.connection', false, true);
+    this.isRateLimited = false;
+    this.refreshInterval = setInterval(
+        () => this.main(),
+        this.config.refreshInterval * 60 * 1000,
+    );
 
-				this.log.info("Anzahl von Stations: " + numberOfRecords);  
-				
-				for (const record of response.data.page.records) {
-					const stationId = record.id;
-					this.stationIds.push(stationId);  
-					this.log.info("Station ID: " + stationId);
+    try {
+      // Liste eingetragene Stations auf
+      const body = JSON.stringify({pageNo: 1, pageSize: 10});
+      const path = '/v1/api/userStationList';
+      const response = await this.apiCall(path, body);
+      if (response && response.success == true) {
+        // Gibt die Anzahl der Stations aus
+        const numberOfRecords = response.data.page.records.length;
 
-					await this.setObjectNotExistsAsync(stationId, {
-						type: "device",
-						common: {
-							name: "stationID",
-						},
-						native: {},
-					});
-				}
-			}
-			
-		} catch (error) {
-            this.log.error(error.toString());
+        this.log.info('Anzahl von Stations: ' + numberOfRecords);
+
+        for (const record of response.data.page.records) {
+          const stationId = record.id;
+          this.stationIds.push(stationId);
+          this.log.info('Station ID: ' + stationId);
+
+          await this.setObjectNotExistsAsync(stationId, {
+            type: 'device',
+            common: {
+              name: 'stationID',
+            },
+            native: {},
+          });
         }
-
-        this.main();
+      }
+    } catch (error) {
+      this.log.error(error.toString());
     }
 
-    async main() {
-		if (this.isRateLimited) {
-			this.log.warn("Skipping due to rate limit. Will retry later.");
-			return;
-		}
+    this.main();
+  }
 
-		for (const stationId of this.stationIds) {  
-			try {
-				// Hole Daten von Station
-				const body = JSON.stringify({ id: stationId });
-				const path = '/v1/api/stationDetail';
-				const response = await this.apiCall(path, body);
+  async main() {
+    if (this.isRateLimited) {
+      this.log.warn('Skipping due to rate limit. Will retry later.');
+      return;
+    }
 
-				if (response && response.success == true) {
-					const allowedKeys = this.config.allowedKeys;
-					//const allowedKeys = ["batteryPower", "batteryPercent", "psum", "totalLoadPower", "power"];
-					const responseData = response.data;
+    for (const stationId of this.stationIds) {
+      try {
+        // Hole Daten von Station
+        const body = JSON.stringify({id: stationId});
+        const path = '/v1/api/stationDetail';
+        const response = await this.apiCall(path, body);
 
-					for (let key in responseData) {
-						if (responseData.hasOwnProperty(key)) {
-							// Überprüfen, ob der Wert gespeichert werden soll
-							if (allowedKeys.includes(key)) {
-								const statePath = `${stationId}.${key}`;
-								const value = responseData[key];
+        if (response && response.success == true) {
+          const allowedKeys = this.config.allowedKeys;
+          const responseData = response.data;
 
-								// Stellen Sie sicher, dass der State existiert, bevor Sie ihn setzen
-								await this.setObjectNotExistsAsync(statePath, {
-									type: "state",
-									common: {
-										name: key,
-										type: typeof value,  // Der Typ wird automatisch auf Grundlage des Wertes bestimmt
-										role: "state",
-										read: true,
-										write: false,
-									},
-									native: {},
-								});
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              // Überprüfen, ob der Wert gespeichert werden soll
+              if (allowedKeys.includes(key)) {
+                const statePath = `${stationId}.${key}`;
+                const value = responseData[key];
 
-								// Setzen Sie den Wert des State
-								await this.setStateAsync(statePath, { val: value, ack: true });
-							}
-						}
-					}
-				}
+                // Stellt sicher, dass der State existiert
+                await this.setObjectNotExistsAsync(statePath, {
+                  type: 'state',
+                  common: {
+                    name: key,
+                    type: typeof value,
+                    role: 'state',
+                    read: true,
+                    write: false,
+                  },
+                  native: {},
+                });
 
-			} catch (error) {
-				this.log.error(error.toString());
-			}
-		}
-
-		this.subscribeStates("*");
-	}
-
-	
-	async apiCall (apiPath, apiBody) {
-		try {
-            const apiKey = this.config.apiKey;
-            const apiSecret = this.config.apiSecret;
-			const apiUrl = this.config.apiUrl
-            const contentMd5 = this.getDigest(apiBody);
-            const date = this.getGMTTime();
-            const param = `POST\n${contentMd5}\napplication/json\n${date}\n${apiPath}`;
-            const sign = this.hmacSHA1Encrypt(param, apiSecret);
-            const url = apiUrl + apiPath;
-            this.log.info("Url-Call: " + url);
-            const response = await axios.post(url, apiBody, {
-                headers: {
-                    'Content-type': 'application/json;charset=UTF-8',
-                    'Authorization': `API ${apiKey}:${sign}`,
-                    'Content-MD5': contentMd5,
-                    'Date': date,
-                },
-            });
-    
-            this.log.debug("Resonse " + apiPath + ": " + JSON.stringify(response.data));
-			this.setState("info.connection", true, true);
-			return response.data
-
-        } catch (error) {
-			if (error.response && error.response.status === 429) {
-				this.log.warn("Rate limit reached. Retrying in 5 minutes...");
-				this.isRateLimited = true;
-				setTimeout(() => {
-					this.isRateLimited = false;
-				}, 5 * 60 * 1000);  // 5 Minuten
-			} else {
-				// andere Fehlerarten
-				this.log.error(error.toString());
-			}
-			this.setState("info.connection", false, true);
-			return null;  // Rückgabe von null, wenn ein Fehler auftritt
+                // Setzen Sie den Wert des State
+                await this.setStateAsync(statePath, {val: value, ack: true});
+              }
+            }
+          }
         }
-		
-	}
-
-    hmacSHA1Encrypt(encryptText, apiSecret) {
-        const hmac = crypto.createHmac('sha1', apiSecret);
-        hmac.update(encryptText, 'utf8');
-        return hmac.digest('base64');
+      } catch (error) {
+        this.log.error(error.toString());
+      }
     }
 
-    getGMTTime() {
-        return new Date().toUTCString();
-    }
+    this.subscribeStates('*');
+  }
 
-    getDigest(test) {
-        const hash = crypto.createHash('md5');
-        hash.update(test);
-        return hash.digest('base64');
-    }
 
-    onStateChange(id, state) {
-        if (state) {
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            this.log.info(`state ${id} deleted`);
-        }
-    }
+  async apiCall(apiPath, apiBody) {
+    try {
+      const apiKey = this.config.apiKey;
+      const apiSecret = this.config.apiSecret;
+      const apiUrl = this.config.apiUrl;
+      const contentMd5 = this.getDigest(apiBody);
+      const date = this.getGMTTime();
+      const param =
+        'POST\n' +
+        `${contentMd5}\n` +
+        'application/json\n' +
+        `${date}\n` +
+        `${apiPath}`;
 
-    onUnload(callback) {
-        try {
-            clearInterval(this.refreshInterval);
-			this.setState("info.connection", false, true);
-        } catch (e) {
-            callback();
-        }
+      const sign = this.hmacSHA1Encrypt(param, apiSecret);
+      const url = apiUrl + apiPath;
+      this.log.info('Url-Call: ' + url);
+      const response = await axios.post(url, apiBody, {
+        headers: {
+          'Content-type': 'application/json;charset=UTF-8',
+          'Authorization': `API ${apiKey}:${sign}`,
+          'Content-MD5': contentMd5,
+          'Date': date,
+        },
+      });
+
+      this.log.debug('Resonse ' + apiPath + ': ' +
+        JSON.stringify(response.data));
+      this.setState('info.connection', true, true);
+      return response.data;
+    } catch (error) {
+      if (error.response && error.response.status === 429) {
+        this.log.warn('Rate limit reached. Retrying in 5 minutes...');
+        this.isRateLimited = true;
+        setTimeout(() => {
+          this.isRateLimited = false;
+        }, 5 * 60 * 1000); // 5 Minuten
+      } else {
+        // andere Fehlerarten
+        this.log.error(error.toString());
+      }
+      this.setState('info.connection', false, true);
+      return null; // Rückgabe von null, wenn ein Fehler auftritt
     }
+  }
+
+  hmacSHA1Encrypt(encryptText, apiSecret) {
+    const hmac = crypto.createHmac('sha1', apiSecret);
+    hmac.update(encryptText, 'utf8');
+    return hmac.digest('base64');
+  }
+
+  getGMTTime() {
+    return new Date().toUTCString();
+  }
+
+  getDigest(test) {
+    const hash = crypto.createHash('md5');
+    hash.update(test);
+    return hash.digest('base64');
+  }
+
+  onStateChange(id, state) {
+    if (state) {
+      this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+    } else {
+      this.log.info(`state ${id} deleted`);
+    }
+  }
+
+  onUnload(callback) {
+    try {
+      clearInterval(this.refreshInterval);
+      this.setState('info.connection', false, true);
+    } catch (e) {
+      callback();
+    }
+  }
 }
 
 if (require.main !== module) {
-    module.exports = (options) => new SolisCloud(options);
+  module.exports = (options) => new SolisCloud(options);
 } else {
-    new SolisCloud();
+  new SolisCloud();
 }
